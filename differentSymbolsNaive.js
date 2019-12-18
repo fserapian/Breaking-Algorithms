2448d1e1df04a52d1d2dd0b3053e5506b894a082
Fadi Serapian <fadi.serapian@gmail.com>
2019-12-17 21:36:05 -0500

    Added differentSymbolNaive algorithm

---
+++ differentSymbolsNaive.js
@@ -0,0 +1,42 @@
+/********* DIFFERENT SYMBOLS NAIVE ***********/
+/*
+	Given a string, find the number of different characters in it.
+
+	Example:
+	For s = 'cabca', differenctSymbolsNaive(s) = 3.
+
+	There are 3 different characters: a, b and c.
+*/
+
+function differentSymbolsNaive(str) {
+	const uniques = [];
+	for (let i = 0; i < str.length; i++) {
+		if (!uniques.includes(str[i])) {
+			uniques.push(str[i]);
+		}
+	}
+	return uniques.length;
+}
+
+function differentSymbolsNaive2(str) {
+	const uniques = [];
+	const chars = str.split('');
+	chars.forEach(char => {
+		if (!uniques.includes(char)) {
+			uniques.push(char);
+		}
+	});
+	return uniques.length;
+}
+
+function differentSymbolsNaive3(str) {
+	const uniques = new Set();
+	const inputChars = str.split('');
+	inputChars.forEach(char => uniques.add(char));
+
+	return uniques.size;
+}
+
+console.log(differentSymbolsNaive('cabca'));
+console.log(differentSymbolsNaive2('cabca'));
+console.log(differentSymbolsNaive3('cabca'));
