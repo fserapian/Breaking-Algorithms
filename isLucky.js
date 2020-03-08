59e2e2c72f697797137049ff35a326b26d0d9d9d
Fadi Serapian <fadi.serapian@gmail.com>
2020-03-08 18:18:58 -0400

    add isLucky two additional methods

--- isLucky.js
+++ isLucky.js
@@ -27,5 +27,46 @@ function isLucky(n) {
 	return firstHalfSum === secondHalfSum;
 }
 
+function isLucky1(n) {
+	let firstHalfSum = 0,
+		secondHalfSum = 0;
+
+	const numArr = n.toString().split('');
+
+	numArr.forEach((num, i) => {
+		if (i < numArr.length / 2) {
+			firstHalfSum += +num; // similar to parseInt(num)
+		} else {
+			secondHalfSum += +num;
+		}
+	})
+
+	return firstHalfSum === secondHalfSum;
+}
+
+function isLucky2(n) {
+	const luckyNum = n.toString();
+
+	const half = luckyNum.length / 2;
+
+	const firstHalf = luckyNum.substring(0, half);
+	const secondHalf = luckyNum.substring(half);
+
+	const firstHalfSum = firstHalf.split('').reduce((acc, elem) => acc + parseInt(elem), 0);
+	const secondHalfSum = secondHalf.split('').reduce((acc, elem) => acc + parseInt(elem), 0);
+
+	return firstHalfSum === secondHalfSum;
+}
+
 console.log(isLucky(1230));
 console.log(isLucky(239017));
+console.log(isLucky1(1230));
+console.log(isLucky1(239017));
+console.log(isLucky2(1230));
+console.log(isLucky2(239017));
+
+
+
+
+
+
