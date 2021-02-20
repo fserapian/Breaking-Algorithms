174dd175d88f0c2fe1fcd492a91090bcf4d6793b
Fadi Serapian <fadi.serapian@gmail.com>
2021-02-20 12:07:34 -0500

    update almostIncreasingSequence.js

--- almostIncreasingSequence.js
+++ almostIncreasingSequence.js
@@ -3,7 +3,7 @@
 // [1, 3, 2] === true    // we can remove 3 to get an increasing sequence
 // [1, 3, 2, 1] === false  // we can't get a sequence by removing a number
 
-function almostIncreasingSequence(arr) {
+function almostIncreasingSequence0(arr) { // not ideal
 	let count = 0;
 
 	for (let i = 0; i < arr.length; i++) {
@@ -18,4 +18,21 @@ function almostIncreasingSequence(arr) {
 	return count <= 1;
 }
 
-console.log(almostIncreasingSequence([1, 2, 3, 5, 4]));
+function almostIncreasingSequence(arr) {
+	let count = 0;
+
+	for (let i = 0; i < arr.length - 1; i++) {
+		if (arr[i + 1] <= arr[i]) {
+			count++;
+		}
+	}
+
+	if (count <= 1) {
+		return true;
+	}
+
+	return false;
+}
+
+console.log(almostIncreasingSequence([1, 3, 2, 1]));
+console.log(almostIncreasingSequence([1, 3, 2]));
