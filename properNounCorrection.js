591a679fe311fed19906b226d72e757b8604c2bd
Fadi Serapian <fadi.serapian@gmail.com>
2021-04-17 14:11:00 -0400

    update properNounCorrection.js

--- properNounCorrection.js
+++ properNounCorrection.js
@@ -20,7 +20,23 @@ function properNounCorrection(noun) {
 // One liner
 const properNounCorrection1 = noun => noun[0].toUpperCase() + noun.slice(1).toLowerCase();
 
+// Another solution
+const properNounCorrection2 = noun => noun.split('').map((n, i) => {
+    if (i === 0) {
+        return n.toUpperCase();
+    } else {
+        return n.toLowerCase();
+    }
+}).join('');
+
+// One liner
+const properNounCorrection3 = noun => noun.split('').map((n, i) => i === 0 ? n.toUpperCase() : n.toLowerCase()).join('');
+
 console.log(properNounCorrection('pARIS'));
 console.log(properNounCorrection('John'));
 console.log(properNounCorrection1('pARIS'));
 console.log(properNounCorrection1('John'));
+console.log(properNounCorrection2('pARIS'));
+console.log(properNounCorrection2('John'));
+console.log(properNounCorrection3('pARIS'));
+console.log(properNounCorrection3('John'));
