db4ae823a2f75e22d6c2b77953eb4299ce70e36c
Fadi Serapian <fadi.serapian@gmail.com>
2022-01-05 01:02:47 -0500

    update missingNumber.js

--- missingNumber.js
+++ missingNumber.js
@@ -1,22 +1,33 @@
 /************* MISSING NUMBER ************/
 /*
-	Ex: if we have and array arr = [1, 8, 6, 4, 5, 2, 7];
-	missingNumber(arr) = 3;
+  Ex: if we have and array arr = [1, 8, 6, 4, 5, 2, 7];
+  missingNumber(arr) = 3;
 */
 
-
 function missingNumber(arr) {
-	let sorted = arr.sort((a, b) => a - b);
+  let sorted = arr.sort((a, b) => a - b);
 
-	for (let i = 0; i < sorted.length - 1; i++) {
-		if (arr[i] !== arr[i + 1] - 1) {
-			return arr[i] + 1;
-		}
-	}
+  if (arr[0] === 2) return 1;
 
-	return 'No missing number';
+  for (let i = 0; i < sorted.length - 1; i++) {
+    if (arr[i] !== arr[i + 1] - 1) {
+      return arr[i] + 1;
+    }
+  }
 
+  return null;
 }
 
-const arr = [1, 5, 4, 3, 2, 7];
+function missingNumber1(arr) {
+  for (let i = 1; i <= arr.length; i++) {
+    if (!arr.includes(i)) {
+      return i;
+    }
+  }
+
+  return null;
+}
+
+const arr = [5, 4, 3, 2, 6, 7];
 console.log(missingNumber(arr));
+console.log(missingNumber1(arr));
