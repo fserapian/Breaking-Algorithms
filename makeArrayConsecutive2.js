05f3358aaacf6c0c9792d91cead25b27c7ab0d23
Fadi Serapian <fadi.serapian@gmail.com>
2022-01-11 01:15:45 -0500

    update makeArrayConsecutive2.js

--- makeArrayConsecutive2.js
+++ makeArrayConsecutive2.js
@@ -1,48 +1,46 @@
 /*************************** MAKE ARRAY CONSECUTIVE 2 *********************/
 /*
-	Ratiorg got statues of different sizes as a present from CodeMaster for
-	his birthday, each statue having a none-negative integer size.
-	Since he likes to make things perfect, he wants to arrange them
-	from smallest to largest so that each statue will be bigger than
-	the previous one exactly by 1.
-	He may need additional statues to be able to accomplish that.
-	Help him figure out the minimum number of additional statues needed.
+    Ratiorg got statues of different sizes as a present from CodeMaster for
+    his birthday, each statue having a none-negative integer size.
+    Since he likes to make things perfect, he wants to arrange them
+    from smallest to largest so that each statue will be bigger than
+    the previous one exactly by 1.
+    He may need additional statues to be able to accomplish that.
+    Help him figure out the minimum number of additional statues needed.
 
-	Example:
-	For statues = [6, 2, 3, 8], the output should be
-	makeArrayConsecutive2(statues) = 3.
+    Example:
+    For statues = [6, 2, 3, 8], the output should be
+    makeArrayConsecutive2(statues) = 3.
 
-	Ratiorg needs statues of sizes 4, 5 and 7.
+    Ratiorg needs statues of sizes 4, 5 and 7.
  */
 
 function makeArrayConsecutive2(statues) {
-	let count = 0;
-	const sorted = statues.sort((a, b) => a - b);
+    let count = 0;
+    const sorted = statues.sort((a, b) => a - b);
 
-	for (let i = 1; i < sorted.length; i++) {
-		if (sorted[i] - sorted[i - 1] !== 1) {
-			count += (sorted[i] - 1 - sorted[i - 1]);
-		}
-	}
-	return count;
+    for (let i = 1; i < sorted.length; i++) {
+        if (sorted[i] - sorted[i - 1] !== 1) {
+            count += (sorted[i] - sorted[i - 1] - 1);
+        }
+    }
+    return count;
 }
 
+// Better
 function makeArrayConsecutive22(statues) {
-	let count = 0;
-	const sorted = statues.sort((a, b) => a - b);
+    let count = 0;
+    const sorted = statues.sort((a, b) => a - b);
 
-	const min = sorted[0];
-	const max = sorted[sorted.length - 1];
+    const min = sorted[0];
+    const max = sorted[sorted.length - 1];
 
-	for (let i = min; i < max; i++) {
-		// if (statues.indexOf(i) === -1) {
-		// 	count++;
-		// }
-		if (!statues.includes(i)) {
-			count++;
-		}
-	}
-	return count;
+    for (let i = min; i < max; i++) {
+        if (!statues.includes(i)) {      // if (statues.indexOf(i) === -1)
+            count++;
+        }
+    }
+    return count;
 }
 
 console.log(makeArrayConsecutive2([6, 2, 3, 8]));
