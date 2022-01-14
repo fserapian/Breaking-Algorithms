cc013fa2634d30e567cb903791096eec1f08ef34
Fadi Serapian <fadi.serapian@gmail.com>
2022-01-14 00:21:32 -0500

    update longestDigitsPrefix.js

--- longestDigitsPrefix.js
+++ longestDigitsPrefix.js
@@ -1,47 +1,53 @@
 /*************************** LONGEST DIGITS PREFIX ******************************/
 /*
-	Given a string, output its longest prefix which contains only digits.
+    Given a string, output its longest prefix which contains only digits.
 
-	Example:
-	For inputString = '123aa1',
-	the output should be longestDigitsPrefix(inputString) = '123'
+    Example:
+    For inputString = '123aa1',
+    the output should be longestDigitsPrefix(inputString) = '123'
  */
 
 function longestDigitsPrefix(inputString) {
-	decimals = '123456789';
-	let res = '';
-	for (let i = 0; i < inputString.length; i++) {
-		if (decimals.includes(inputString[i])) {
-			res += inputString[i];
-		} else {
-			break;
-		}
-	}
-	return res;
+    decimals = '123456789';
+    let res = '';
+
+    for (let i = 0; i < inputString.length; i++) {
+        if (!decimals.includes(inputString[i])) {
+            break;
+        }
+
+        res += inputString[i];
+    }
+
+    return res;
 }
 
 function longestDigitsPrefix1(inputString) {
-	let res = '';
-	for (let i = 0; i < inputString.length; i++) {
+    let res = '';
 
-		if (!parseInt(inputString[i])) { 
-			break;
-		}
+    for (let i = 0; i < inputString.length; i++) {
+        if (!parseInt(inputString[i])) { 
+            break;
+        }
 
-		res += inputString[i];
-	}
-	return res;
+        res += inputString[i];
+    }
+
+    return res;
 }
 
 function longestDigitsPrefix2(inputString) {
-	let res = '';
-	for (let char of inputString) {
-		if (isNaN(parseInt(char))) {
-			break;
-		}
-		res += char;
-	}
-	return res;
+    let res = '';
+
+    for (let char of inputString) {
+        if (isNaN(parseInt(char))) {
+            break;
+        }
+
+        res += char;
+    }
+
+    return res;
 }
 
 console.log(longestDigitsPrefix('123aa1'));
