1e45b3c67cb911cf53712ca41b4cc389fafcfa8c
Fadi Serapian <fadi.serapian@gmail.com>
2022-02-01 00:33:41 -0500

    Update LargestNumberBasedOfN.js

--- LargestNumberBasedOfN.js
+++ LargestNumberBasedOfN.js
@@ -1,22 +1,26 @@
 /******************* LARGEST NUMBER BASED OF N *******************/
 /*
-	Example:
-		For n = 2, largestNumber(n) = 99
+    Example:
+        For n = 2, largestNumber(n) = 99
  */
 
 function largestNumber(n) {
-	let str = '';
-	for (let i = 0; i < n; i++) {
-		str += '9';
-	}
-
-	return parseInt(str);
+    let str = '';
+    for (let i = 0; i < n; i++) {
+        str += '9';
+    }
+    return parseInt(str);
 }
 
 function largestNumber1(n) {
-	const str = '9'.repeat(n);
-	return parseInt(str);
+    const largestNumberString = '9'.repeat(n);
+    return parseInt(largestNumberString);
 }
+
+// One liner
+const largestNumber2 = (n) => parseInt('9'.repeat(n));
 
 console.log(largestNumber(2));
 console.log(largestNumber1(2));
+console.log(largestNumber2(2));
+console.log(largestNumber2(5));
