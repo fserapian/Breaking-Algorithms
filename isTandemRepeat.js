8a19a8022ea779680b38301bce339f9c80b62934
Fadi Serapian <fadi.serapian@gmail.com>
2022-02-08 00:02:03 -0500

    Update isTandemRepeat.js

--- isTandemRepeat.js
+++ isTandemRepeat.js
@@ -1,24 +1,38 @@
 /**************************** IS TANDEM REPEAT **************************/
 /*
-	Determine wether the given string can be obtained by one concatenation
-	of some string to itself.
+    Determine wether the given string can be obtained by one concatenation
+    of some string to itself.
 
-	For inputString = 'tandemtandem', isTandemRepeat(inputString) = true,
-	For inputString = 'qqq', isTandemRepeat(inputString) = false,
-	For inputString = '2w2ww', isTandemRepeat(inputString) = false
+    For inputString = 'tandemtandem', isTandemRepeat(inputString) = true,
+    For inputString = 'qqq', isTandemRepeat(inputString) = false,
+    For inputString = '2w2ww', isTandemRepeat(inputString) = false
  */
 
 function isTandemRepeat(inputString) {
-	if (inputString.length % 2 !== 0) {
-		return false;
-	}
+    if (inputString.length % 2 !== 0) {
+        return false;
+    }
 
-	const strLength = inputString.length;
-	const firstPart = inputString.slice(0, strLength / 2);
-	const secondPart = inputString.slice(strLength / 2);
+    const strLength = inputString.length;
+    const firstPart = inputString.slice(0, strLength / 2);
+    const secondPart = inputString.slice(strLength / 2);
 
-	return firstPart === secondPart;
+    return firstPart === secondPart;
 }
 
-const str = 'thetruththetrutg';
-console.log(isTandemRepeat(str));
+// Shorter
+const isTandemRepeat1 = (str) => {
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+
+    return str.slice(0, str.length / 2) === str.slice(str.length / 2);
+};
+
+const str1 = 'thetruththetrutg';
+const str2 = 'tandemtandem'
+
+console.log(isTandemRepeat(str1));
+console.log(isTandemRepeat(str2));
+console.log(isTandemRepeat1(str1));
+console.log(isTandemRepeat1(str2));
