052c4132b70419d0e491cfba9b2a6387db7b2c5f
Fadi Serapian <fadi.serapian@gmail.com>
2022-02-16 00:56:02 -0500

    Update incorrectPasscodeAttempts.js

--- incorrectPasscodeAttempts.js
+++ incorrectPasscodeAttempts.js
@@ -1,79 +1,96 @@
 /************************ INCORRECT PASSCODE ATTEMPTS ************************/
 /*
-	One very Important User (VIU) has a Very Confidential Document (VCD) stored
-	on his Dropbox account. He doesn't let anyone see the VCD, especially his
-	roommates who often have access to his devices.
+    One very Important User (VIU) has a Very Confidential Document (VCD) stored
+    on his Dropbox account. He doesn't let anyone see the VCD, especially his
+    roommates who often have access to his devices.
 
-	Opening the Dropbox mobile app on the VIU's tablet requires a four-digit passcode.
-	To ensure the confidentiality of the stored information, the device is locked out
-	of Dropbox after 10 consecutive failed passcode attempts.
+    Opening the Dropbox mobile app on the VIU's tablet requires a four-digit passcode.
+    To ensure the confidentiality of the stored information, the device is locked out
+    of Dropbox after 10 consecutive failed passcode attempts.
 
-	We need to implement a function that given an array of attempts made throughout
-	the day and the correct passcode checks to see if the device should be locked,
-	i.e. 10 or more consecutive failed passcode attempts were made.
+    We need to implement a function that given an array of attempts made throughout
+    the day and the correct passcode checks to see if the device should be locked,
+    i.e. 10 or more consecutive failed passcode attempts were made.
 
-	Example:
-	For passcode = '1111' and
-	attempts = ['1111', '4444', '9999', '3333', '8888', '2222',
-				 '7777', '0000', '6666', '7285', '5555', '1111']
+    Example:
+    For passcode = '1111' and
+    attempts = ['1111', '4444', '9999', '3333', '8888', '2222',
+                 '7777', '0000', '6666', '7285', '5555', '1111']
 
-	incorrectPasscodeAttempts(passcode, attempts) = true
+    incorrectPasscodeAttempts(passcode, attempts) = true
 
-	The first attempt is correct, so the user must have successfully logged in.
-	However, the next 10 consecutive attempts are incorrect, so the device should be
-	locked. Thus, the output should be true.
+    The first attempt is correct, so the user must have successfully logged in.
+    However, the next 10 consecutive attempts are incorrect, so the device should be
+    locked. Thus, the output should be true.
 */
 
 function incorrectPasscodeAttempts(passcode, attempts) {
-	let failedAttempts = 0;
+    let failedAttempts = 0;
 
-	for (let i = 0; i < attempts.length; i++) {
+    for (let i = 0; i < attempts.length; i++) {
 
-		if (attempts[i] === passcode) {
-			failedAttempts = 0;
-		}
+        if (attempts[i] === passcode) {
+            failedAttempts = 0;
+        }
 
-		if (attempts[i] !== passcode) {
-			failedAttempts++;
-		}
+        if (attempts[i] !== passcode) {
+            failedAttempts++;
+        }
 
-		if (failedAttempts === 10) {
-			return true;
-		}
-	}
+        if (failedAttempts === 10) {
+            return true;
+        }
+    }
 
-	return false;
+    return false;
 }
 
 function incorrectPasscodeAttempts1(passcode, attempts) {
-	let failedAttempts = 0;
+    let failedAttempts = 0;
 
-	for (let attempt of attempts) {
+    for (let attempt of attempts) {
+        if (attempt === passcode) {
+            failedAttempts = 0;
+        } else {
+            failedAttempts++;
+        }
 
-		if (attempt === passcode) {
-			failedAttempts = 0;
-		} else {
-			failedAttempts++;
-		}
+        if (failedAttempts === 10) {
+            return true;
+        }
+    }
 
-		// failedAttempts = attempt === passcode ? 0 : failedAttempts++; // ???
+    return false;
+}
 
-		if (failedAttempts === 10) {
-			return true;
-		}
-	}
+function incorrectPasscodeAttempts2(passcode, attempts) {
+    let failedAttemptsCount = 0;
+    let isLocked = false;
 
-	return false;
+    for (let i = 0; i < attempts.length; i++) {
+       failedAttemptsCount = attempts[i] === passcode ? 0 : failedAttemptsCount + 1;
+
+       if (failedAttemptsCount === 10) {
+           isLocked = true;
+       }
+    }
+
+    return isLocked;
 }
 
 
-const passcode = '1111';
-const attempts = ['1111', '4444', '9999', '3333', '8888', '2222',
-				 '7777', '0000', '6666', '7285', '5555', '1111'];
+const passcode1 = '1111';
+const attempts1 = ['1111', '4444', '9999', '3333', '8888', '2222',
+    '7777', '0000', '6666', '7285', '5555', '1111'];
 
 const passcode2 = '2222';
 const attempts2 = ['3333', '2222', '1111', '2222'];
-console.log(incorrectPasscodeAttempts1(passcode, attempts));
+
+console.log(incorrectPasscodeAttempts(passcode1, attempts1));
+console.log(incorrectPasscodeAttempts(passcode2, attempts2));
+console.log('---------------------------------------------');
+console.log(incorrectPasscodeAttempts1(passcode1, attempts1));
 console.log(incorrectPasscodeAttempts1(passcode2, attempts2));
-
-
+console.log('---------------------------------------------');
+console.log(incorrectPasscodeAttempts2(passcode1, attempts1));
+console.log(incorrectPasscodeAttempts2(passcode2, attempts2));
