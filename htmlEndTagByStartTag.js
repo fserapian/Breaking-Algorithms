ece01545261c83a104d10dc2cb66648f3ca20000
Fadi Serapian <fadi.serapian@gmail.com>
2022-02-17 00:08:30 -0500

    Update htmlEndTagByStartTag.js

--- htmlEndTagByStartTag.js
+++ htmlEndTagByStartTag.js
@@ -1,29 +1,28 @@
 /******************** HTML END TAG BY START TAG ********************/
 /*
-	You are implementing your own HTML editor. To make it more comfortable for
-	developers you would like to add auto-completion feature to it.
-	Given the starting HTML tag, find the appropriate end tag which your editor
-	should propose.
+    You are implementing your own HTML editor. To make it more comfortable for
+    developers you would like to add auto-completion feature to it.
+    Given the starting HTML tag, find the appropriate end tag which your editor
+    should propose.
 
-	Examples:
-	For startTag = "<button type='button' disabled>",
-		htmlEndTagByStartTag(startTag) = '</button>'
+    Examples:
+    For startTag = "<button type='button' disabled>",
+        htmlEndTagByStartTag(startTag) = '</button>'
 
-	For startTag = "<i>",
-		htmlEndTagByStartTag(startTag) = '</i>'		
-
+    For startTag = "<i>",
+        htmlEndTagByStartTag(startTag) = '</i>'
 */
 
 function htmlEndTagByStartTag(startTag) {
-	endTag = '</';
-	for (let i = 1; i < startTag.length; i++) {
-		if (startTag[i] === ' ' || startTag[i] === '>') {
-			break;
-		}
-		endTag += startTag[i];
-	}
+    endTag = '</';
+    for (let i = 1; i < startTag.length; i++) {
+        if (startTag[i] === ' ' || startTag[i] === '>') {
+            break;
+        }
+        endTag += startTag[i];
+    }
 
-	return endTag + '>';
+    return endTag + '>';
 }
 
 console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
