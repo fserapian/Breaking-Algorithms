daf63a9b74d4691e2a255da7bc9027c1d87c0741
Fadi Serapian <fadi.serapian@gmail.com>
2022-03-22 00:49:51 -0400

    Update extractEachKth.js

--- extractEachKth.js
+++ extractEachKth.js
@@ -1,45 +1,44 @@
 /************** EXTRACT EACH Kth *************/
 /*
-	Given an array of integers, remove every kth element from it.
-	
-	Ex: For inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] and k = 3.
+    Given an array of integers, remove every kth element from it.
+    
+    Ex: For inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] and k = 3.
 
-	extractEachKth(inputArray) = [1, 2, 4, 5, 7, 8, 10]
+    extractEachKth(inputArray) = [1, 2, 4, 5, 7, 8, 10]
 
 */
 
 function extractEachKth(inputArr, k) {
-	let step = k;
-	return inputArr.filter((num, i) => {
-		if (i !== step - 1) {
-			return num;
-		}
-		step = step + k;
-	});
+    let step = k;
+    return inputArr.filter((num, i) => {
+        if (i !== step - 1) {
+            return num;
+        }
+        step = step + k;
+    });
+}
+
+function extractEachKth1(arr, k) {
+    let step = k;
+    let res = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i !== step - 1) {
+            res.push(arr[i]);
+        } else {
+            step += k;
+        }
+    }
+    return res;
 }
 
 function extractEachKth2(arr, k) {
-	const newArr = arr;
-	let step = k;
-
-	for (let i = 0; i < newArr.length; i++) {
-		if (i === step - 1) {
-			newArr.splice(i, 1);
-
-			step += k - 1;
-		}
-	}
-	return newArr;
-}
-
-function extractEachKth3(arr, k) {
-	return arr.filter((num, i) => (i + 1) % k !== 0);
+    return arr.filter((num, i) => (i + 1) % k !== 0);
 }
 
 const inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const k = 3;
 
-// console.log(extractEachKth(inputArray, k));
-// console.log(extractEachKth2(inputArray, k));
-console.log(extractEachKth3(inputArray, k));
-
+console.log(extractEachKth(inputArray, k));
+console.log(extractEachKth1(inputArray, k));
+console.log(extractEachKth2(inputArray, k));
