e21a8118dd9fa9f6c24678ffa1a91600da590b9b
Fadi Serapian <fadi.serapian@gmail.com>
2022-03-25 00:13:18 -0400

    Update evenDigitsOnly.js

--- evenDigitsOnly.js
+++ evenDigitsOnly.js
@@ -1,47 +1,46 @@
 /************** EVEN DIGITS ONLY *****************/
 /*
-	Check if all digits of the given integer are even
+    Check if all digits of the given integer are even
 
-	Ex:
-		For n = 24682, evenDigitsOnly(n) = true;
-		For n = 68423, evenDigitsOnly(n) = false;
+    Ex:
+        For n = 24682, evenDigitsOnly(n) = true;
+        For n = 68423, evenDigitsOnly(n) = false;
 */
 
 function evenDigitsOnly(number) {
-	const strArr = number.toString().split('');
+    const strArr = number.toString().split('');
 
-	let isEven = true;
-	strArr.forEach(strNum => {
-		if (parseInt(strNum) % 2 !== 0) {
-			isEven = false;
-		}
-	});
+    let isEven = true;
+    strArr.forEach((strNum) => {
+        if (parseInt(strNum) % 2 !== 0) {
+            isEven = false;
+        }
+    });
 
-	return isEven;
+    return isEven;
 }
 
-function evenDigitsOnly1(number) {
-	const strArr = number.toString().split('');
+function evenDigitsOnly1(num) {
+    const strArr = num.toString().split('');
 
-	for (let i = 0; i < strArr.length; i++) {
-		if (parseInt(strArr[i]) % 2 !== 0) { // Catching an odd number
-			return false;
-		}
-	}
+    for (let i = 0; i < strArr.length; i++) {
+        if (parseInt(strArr[i]) % 2 !== 0) {
+            return false;
+        }
+    }
 
-	return true;
+    return true;
 }
 
-function evenDigitsOnly2(number) {
-	const strArr = number.toString().split('');
-
-	return strArr.every(strNum => parseInt(strNum) % 2 === 0);
+function evenDigitsOnly2(num) {
+    const strArr = num.toString().split('');
+    return strArr.every((strNum) => parseInt(strNum) % 2 === 0);
 }
 
-console.log(evenDigitsOnly2(44646328));
+// One line
+const evenDigitsOnly3 = (num) => num.toString().split('').every((el) => +el % 2 === 0);
+
+console.log(evenDigitsOnly(44646228));
+console.log(evenDigitsOnly1(44646228));
 console.log(evenDigitsOnly2(44646228));
-
-
-
-
-
+console.log(evenDigitsOnly3(44646228));
