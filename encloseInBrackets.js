953d9f1e1af254300ad6aff8e93654039339e516
Fadi Serapian <fadi.serapian@gmail.com>
2022-03-28 23:58:23 -0400

    Update encloseInBrackets.js

--- encloseInBrackets.js
+++ encloseInBrackets.js
@@ -1,22 +1,29 @@
 /************ ENCLOSE IN BRACKETS ************/
 
 /*
-	Given a string, enclose it in round brackets
+    Given a string, enclose it in round brackets
 */
 
 function encloseInBrackets(str) {
-	const strArr = str.split('');
-	strArr.push(')')
-	strArr.unshift('(');
-	return strArr.join('');
+    const strArr = str.split('');
+    strArr.push(')')
+    strArr.unshift('(');
+    return strArr.join('');
 }
 
 function encloseInBrackets1(str) {
-	return `(${str})`;
+    return `(${str})`;
 }
 
 function encloseInBrackets2(str) {
-	return '('.concat(str).concat(')');
+    return '('.concat(str).concat(')');
 }
 
+function encloseInBrackets3(str) {
+    return '(' + str + ')';
+}
+
+console.log(encloseInBrackets('Fadi'));
+console.log(encloseInBrackets1('Fadi'));
 console.log(encloseInBrackets2('Fadi'));
+console.log(encloseInBrackets3('Fadi'));
