e1e8bf7b53671cf7ad42198abc721f11a58284dd
Fadi Serapian <fadi.serapian@gmail.com>
2022-03-31 00:45:24 -0400

    Update domainType.js

--- domainType.js
+++ domainType.js
@@ -1,48 +1,79 @@
 /***************** DOMAIN TYPE ******************/
 /*
-	Ex: If we have an array with different domains
-	const arr = 
-	['en.wiki.com', 'codefights.com', 'happey.net', 'code.info'];
+    Ex: If we have an array with different domains
+    const arr =
+    ['en.wiki.com', 'codefights.com', 'happey.net', 'code.info', 'something'];
 
-	domainType(arr) =
-	['commercial', 'commercial', 'network', 'information']
+    domainType(arr) =
+    ['commercial', 'commercial', 'network', 'information', 'other']
 */
 
 function domainType(domains) {
-	return domains.map(domain => {
-		if (domain.includes('com')) {
-			return 'commercial';
-		} else if (domain.includes('org')) {
-			return 'organization';
-		} else if (domain.includes('net')) {
-			return 'network';
-		} else if (domain.includes('info')) {
-			return 'information';
-		}
-	})
+    return domains.map((domain) => {
+        if (domain.includes('com')) {
+            return 'commercial';
+        }
+
+        if (domain.includes('org')) {
+            return 'organization';
+        }
+
+        if (domain.includes('net')) {
+            return 'network';
+        }
+
+        if (domain.includes('info')) {
+            return 'information';
+        }
+
+        return 'other';
+    });
 }
 
 function domainType1(domains) {
-	const domainTypes = [];
+    return domains.map((domain) => {
+        const parts = domain.split('.')
+        const ext = parts[parts.length - 1];
 
-	for (let i = 0; i < domains.length; i++) {
-		const parts = domains[i].split('.');
-		const ext = parts[parts.length - 1];
-
-		if (ext === 'com') {
-			domainTypes.push('commercial');
-		} else if (ext === 'org') {
-			domainTypes.push('organization');
-		} else if (ext === 'net') {
-			domainTypes.push('network');
-		} else if (ext === 'info') {
-			domainTypes.push('information');
-		}
-	}
-
-	return domainTypes;
+        switch (ext) {
+            case 'com':
+                return 'commercial';
+            case 'org':
+                return 'organization';
+            case 'net':
+                return 'network';
+            case 'info':
+                return 'information';
+            default:
+                return 'other';
+        }
+    });
 }
 
-const arr = ['en.wiki.com', 'codefights.com', 'happey.net', 'code.info'];
-console.log(domainType1(arr));
+function domainType2(domains) {
+    const domainTypes = [];
 
+    for (let i = 0; i < domains.length; i++) {
+        const parts = domains[i].split('.');
+        const ext = parts[parts.length - 1];
+
+        if (ext === 'com') {
+            domainTypes.push('commercial');
+        } else if (ext === 'org') {
+            domainTypes.push('organization');
+        } else if (ext === 'net') {
+            domainTypes.push('network');
+        } else if (ext === 'info') {
+            domainTypes.push('information');
+        } else {
+            domainTypes.push('other');
+        }
+    }
+
+    return domainTypes;
+}
+
+const arr = ['en.wiki.com', 'codefights.com', 'happey.net', 'code.info', 'something'];
+console.log(domainType(arr));
+console.log(domainType1(arr));
+console.log(domainType2(arr));
