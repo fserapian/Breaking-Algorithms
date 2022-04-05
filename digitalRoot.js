fe3ca45d90c7b6c790fbf4ad436a5a79f65ed626
Fadi Serapian <fadi.serapian@gmail.com>
2022-04-05 00:28:10 -0400

    Update digitalRoot.js

--- digitalRoot.js
+++ digitalRoot.js
@@ -1,45 +1,45 @@
 /********************** DIGITAL ROOT ***********************/
 /*
-	A digital root is the recursive sum of all the digits in a number.
-	Given n, take the sum of digits of n.
-	If that value has two digits, continue reducing in this way
-	until a signle-digit number is produced.
-	This is only applicable to the natural numbers.
+    A digital root is the recursive sum of all the digits in a number.
+    Given n, take the sum of digits of n.
+    If that value has two digits, continue reducing in this way
+    until a signle-digit number is produced.
+    This is only applicable to the natural numbers.
 
-	Examples:
+    Examples:
 
-	digitalRoot(16)
-	=> 1 + 6
-	=> 7
+    digitalRoot(16)
+    => 1 + 6
+    => 7
 
-	digitalRoot(942)
-	=> 9 + 4 + 2
-	=> 15 ...
-	=> 1 + 5
-	=> 6
+    digitalRoot(942)
+    => 9 + 4 + 2
+    => 15 ...
+    => 1 + 5
+    => 6
  */
 
 function digitalRoot(n) {
-	while (n > 9) {
+    while (n > 9) {
+        let sum = 0;
 
-		let sum = 0;
-		while (n > 0) {
-			sum += n % 10;
-			n = Math.floor(n / 10);
-		}
+        while (n > 0) {
+            sum += n % 10;
+            n = Math.floor(n / 10);
+        }
 
-		n = sum
-	}
+        n = sum
+    }
 
-	return n;
+    return n;
 }
 
 function digitalRoot1(n) {
-	while (n > 9) {
-		n = n.toString().split('').reduce((sum, num) => sum + +num, 0);
-	}
+    while (n > 9) {
+        n = n.toString().split('').reduce((sum, num) => sum + +num, 0);
+    }
 
-	return n;
+    return n;
 }
 
 console.log(digitalRoot(16));
