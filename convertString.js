0e4dc9e3ebf4124eac895582e9dba6c45461ad0a
Fadi Serapian <fadi.serapian@gmail.com>
2022-04-13 00:33:51 -0400

    Update convertString.js

--- convertString.js
+++ convertString.js
@@ -1,7 +1,7 @@
 /******** CONVERT STRING ********/
 /*
-    You are given the string s. Your friend just asked you it it's possible
-    to change the string from s to a string t by removing some characters from it.
+    You are given the string s. Your friend just asked you if it's possible
+    to change the string from (s) to a string (t) by removing some characters from it.
     You're a pro at programming, so you decided to create a program to determine this.
 
     Example:
@@ -13,14 +13,20 @@
 function convertString(s, t) {
     let str = '';
     let tIndex = 0;
+
     for (let i = 0; i < s.length; i++) {
         if (s[i] === t[tIndex]) {
             str += s[i];
             tIndex++;
+
+            if (str === t) {
+                return true;
+            }
         }
     }
-    return str === t;
+
+    return false;
 }
 
 console.log(convertString('fahasdFdCdodseifigdhsts', 'Codefights'));
-console.log(convertString('fahasdFdCddseifigdhsts', 'Codefights'));
+console.log(convertString('addbya', 'abcd'));
