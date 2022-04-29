81697acc9f8ff1a23aef9d707c4d85fa517b6c12
Fadi Serapian <fadi.serapian@gmail.com>
2022-04-29 01:04:04 -0400

    Update chessBoardCellColor.js

--- chessBoardCellColor.js
+++ chessBoardCellColor.js
@@ -1,49 +1,66 @@
+/********************* CHESS BOARD CELL COLOR **************************/
 /*
-Given two cells on the standard chess board,
-determine whether they have the same color or not.
-Ex: cell1 = 'b1', cell2 = 'd1',
-	chessBoardColor(cell1, cell2) = true.
-	cell1 = 'a1', cell2 = 'h3',
-	chessBoardColor(cell1, cell2) = false.
+  Given two cells on the standard chess board,
+  determine whether they have the same color or not.
+  Ex: cell1 = 'b1', cell2 = 'd1',
+    chessBoardColor(cell1, cell2) = true.
+    cell1 = 'a1', cell2 = 'h3',
+    chessBoardColor(cell1, cell2) = false.
+
+    (w): White
+    (b): Black
+
+    8 (w) (b) (w) (b) (w) (b) (w) (b)
+    7 (b) (w) (b) (w) (b) (w) (b) (w)
+    6 (w) (b) (w) (b) (w) (b) (w) (b)
+    5 (b) (w) (b) (w) (b) (w) (b) (w)
+    4 (w) (b) (w) (b) (w) (b) (w) (b)
+    3 (b) (w) (b) (w) (b) (w) (b) (w)
+    2 (w) (b) (w) (b) (w) (b) (w) (b)
+    1 (b) (w) (b) (w) (b) (w) (b) (w)
+       a   b   c   d   e   f   g   h
 */
 
-// Mapping board
-const board = {
-		'a': 1,
-		'b': 2,
-		'c': 3,
-		'd': 4,
-		'e': 5,
-		'f': 6,
-		'g': 7,
-		'h': 8,
-	};
-
 function chessBoardCellColor(cell1, cell2) {
+  const board = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+  };
 
-	const x1 = board[cell1[0]],
-		y1 = parseInt(cell1[1]),
-		x2 = board[cell2[0]],
-		y2 = parseInt(cell2[1]);
+  const x1 = board[cell1.charAt(0)];
+  const y1 = parseInt(cell1.charAt(1));
+  const x2 = board[cell2.charAt(0)];
+  const y2 = parseInt(cell2.charAt(1));
 
-	return ((x1 % 2 !== 0 && y1 % 2 !== 0 || x1 % 2 === 0 && y1 % 2 === 0)
-		&& (x2 % 2 !== 0 && y2 % 2 !== 0 || x2 % 2 === 0 && y2 % 2 === 0))
-		|| ((x1 % 2 !== 0 && y1 % 2 === 0 || x1 % 2 === 0 && y1 % 2 !== 0)
-		&& (x2 % 2 !== 0 && y2 % 2 === 0 || x2 % 2 === 0 && y2 % 2 !== 0));
+  return ((x1 + y1) % 2) === ((x2 + y2) % 2);
 }
 
 // Another Solution
-function chessBoardCellColor2(cell1, cell2) {
+function chessBoardCellColor1(cell1, cell2) {
+  const board = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+  };
 
-	const symbol1 = (board[cell1[0]] + parseInt(cell1[1])) % 2;
-	const symbol2 = (board[cell2[0]] + parseInt(cell2[1])) % 2;
+  const symbol1 = (board[cell1[0]] + parseInt(cell1[1])) % 2;
+  const symbol2 = (board[cell2[0]] + parseInt(cell2[1])) % 2;
 
-	return symbol1 === symbol2;
+  return symbol1 === symbol2;
 }
 
 console.log(chessBoardCellColor('b1', 'd1'));
-console.log(chessBoardCellColor2('a1', 'h3'));
-
-
-
-
+console.log(chessBoardCellColor1('b1', 'd1'));
+console.log(chessBoardCellColor('a1', 'h3'));
+console.log(chessBoardCellColor1('a1', 'h3'));
