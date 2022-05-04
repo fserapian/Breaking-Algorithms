91fbf3d78f6d88711fc9230c551c0eac04cc3e17
Fadi Serapian <fadi.serapian@gmail.com>
2022-05-04 00:04:00 -0400

    Update checkPalidrome.js

--- checkPalidrome.js
+++ checkPalidrome.js
@@ -1,26 +1,31 @@
+/********************* CHECK PALINDROME **************************/
 /*
-Given a string, check if it is a palindrome.
-Ex:
-inputString = 'aabaa', checkPalindrome(inputString) = true.
-inputString = 'abac', checkPalindrome(inputString) = false.
-inputString = 'a', checkPalindrome(inputString) = true.
+  Given a string, check if it is a palindrome.
+  Ex:
+  inputString = 'aabaa', checkPalindrome(inputString) = true.
+  inputString = 'abac', checkPalindrome(inputString) = false.
+  inputString = 'a', checkPalindrome(inputString) = true.
 */
 
-
 function checkPalindrome(str) {
-	const lowerCaseStr = str.toLowerCase();
+  const lowerCaseStr = str.toLowerCase();
 
-	for (let i = 0; i < lowerCaseStr.length / 2; i++) {
-		if (lowerCaseStr[i] !== lowerCaseStr[lowerCaseStr.length - 1 - i]) {
-			return false;
-		}
-	}
-	return true;
+  for (let i = 0; i < lowerCaseStr.length / 2; i++) {
+    if (lowerCaseStr[i] !== lowerCaseStr[lowerCaseStr.length - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 // Another Solution
-function checkPalindrome2(str) {
-	return str.toLowerCase() === str.toLowerCase().split('').reverse().join('');
+function checkPalindrome1(str) {
+  const lowerCaseString = str.toLowerCase();
+  return lowerCaseString === lowerCaseString.split('').reverse().join('');
 }
 
-console.log(checkPalindrome2('Abba'));
+console.log(checkPalindrome('Abba'));
+console.log(checkPalindrome1('Abba'));
+console.log(checkPalindrome('abac'));
+console.log(checkPalindrome1('abac'));
