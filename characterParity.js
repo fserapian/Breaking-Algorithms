c75e3333c0715c596d4f325085337717553876df
Fadi Serapian <fadi.serapian@gmail.com>
2022-05-09 00:03:57 -0400

    Update characterParity.js

--- characterParity.js
+++ characterParity.js
@@ -1,30 +1,35 @@
+/*************************** CHARACTER PARITY *****************************/
 /*
-Given a character, check if it represents an odd digit,
-and even digit or not a digit at all.
-Ex: For symbol = '5', characterParity(symbol) = 'odd'.
-	For symbol = '8', characterParity(symbol) = 'even'.
-	For symbol = 'q', characterParity(symbol) = 'not a digit'.
+  Given a character, check if it represents an odd digit,
+  and even digit or not a digit at all.
+  Ex: For symbol = '5', characterParity(symbol) = 'odd'.
+    For symbol = '8', characterParity(symbol) = 'even'.
+    For symbol = 'q', characterParity(symbol) = 'not a digit'.
 */
 
 function characterParity(symbol) {
-	const num = parseInt(symbol);
-	if (isNaN(num)) {
-		return 'not a digit';
-	} else {
-		if (num % 2 === 0) {
-			return 'even';
-		} else {
-			return 'odd';
-		}
-	}
+  const num = parseInt(symbol);
+
+  if (isNaN(num)) {
+    return 'not a digit';
+  }
+
+  if (num % 2 === 0) {
+    return 'even';
+  }
+
+  return 'odd';
 }
 
-// Shortcut code
-function characterParity2(symbol) {
-	const num = Number(symbol);
-	return isNaN(num) ? 'not a digit' : num % 2 === 0 ? 'even' : 'odd';
+// Shorter code
+function characterParity1(symbol) {
+  const num = Number(symbol);
+  return isNaN(num) ? 'not a digit' : num % 2 === 0 ? 'even' : 'odd';
 }
 
-console.log(characterParity2('5'));
-console.log(characterParity2('8'));
-console.log(characterParity2('q'));
+console.log(characterParity('5'));
+console.log(characterParity('8'));
+console.log(characterParity('q'));
+console.log(characterParity1('5'));
+console.log(characterParity1('8'));
+console.log(characterParity1('q'));
