870b77b7da0befd2d885f74547bb3d958c54c25e
Fadi Serapian <fadi.serapian@gmail.com>
2022-05-13 00:17:41 -0400

    Update arrayReplace.js

--- arrayReplace.js
+++ arrayReplace.js
@@ -1,15 +1,16 @@
+/********************** ARRAY REPLACE **********************/
 /*
-Given an array of integers, replace all ocurrences of elementToReplace
-with substitutionElement
+  Given an array of integers, replace all ocurrences of elementToReplace
+  with substitutionElement
 
-Ex: arr = [1, 2, 1], elementToReplace = 1, substitutionElement = 5,
-	 arrayReplace(arr, elementToReplace, substitutionElement) = [5, 2, 5];
+  Ex: arr = [1, 2, 1], elementToReplace = 1, substitutionElement = 5,
+    arrayReplace(arr, elementToReplace, substitutionElement) = [5, 2, 5];
 */
 
-
 function arrayReplace(inputArray, elementToReplace, substitutionElement) {
-	return inputArray
-		.map(element => element === elementToReplace ? substitutionElement : element);
+  return inputArray.map((element) => {
+    return element === elementToReplace ? substitutionElement : element;
+  });
 }
 
 console.log(arrayReplace([1, 2, 1], 1, 5));
