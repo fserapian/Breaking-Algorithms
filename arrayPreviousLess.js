4ef31b607055802afe1904b3faffbc84a1d8eeb0
Fadi Serapian <fadi.serapian@gmail.com>
2022-05-16 00:28:16 -0400

    Update arrayPreviousLess.js

--- arrayPreviousLess.js
+++ arrayPreviousLess.js
@@ -1,25 +1,27 @@
+/********************** ARRAY PREVIOUS LESS **********************/
 /*
-Given an array of integers, for each position i,
-search among the previous positions for the last (from the left)
-position that contains a smaller value.
-Store this value at position i in the answer.
-If no such value can be found, store -1 instead.
+  Given an array of integers, for each position i,
+  search among the previous positions for the last (from the left)
+  position that contains a smaller value.
+  Store this value at position i in the answer.
+  If no such value can be found, store -1 instead.
 
-Ex: arr = [3, 5, 2, 4, 5], arrayPreviousLess(arr) = [-1, 3, -1, 2, 4]
+  Ex: arr = [3, 5, 2, 4, 5], arrayPreviousLess(arr) = [-1, 3, -1, 2, 4]
 */
 
 function arrayPreviousLess(arr) {
-	const newArr = [-1];
-	for (let i = 1; i < arr.length; i++) {
-		if (arr[i - 1] < arr[i]) {
-			newArr.push(arr[i - 1]);
-		} else {
-			newArr.push(-1);
-		}
-	}
-	return newArr;
-}
+  const newArr = [-1];
 
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i - 1] < arr[i]) {
+      newArr.push(arr[i - 1]);
+    } else {
+      newArr.push(-1);
+    }
+  }
+
+  return newArr;
+}
 
 console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
 console.log(arrayPreviousLess([8, 3, 2, 9, 10]));
