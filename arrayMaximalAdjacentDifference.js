8c2eab1a5f7b3f30e142694ea3f3f29f5880cd27
Fadi Serapian <fadi.serapian@gmail.com>
2022-05-17 00:08:03 -0400

    Update arrayMaximalAdjacentDifference.js

--- arrayMaximalAdjacentDifference.js
+++ arrayMaximalAdjacentDifference.js
@@ -1,33 +1,35 @@
+/************************ ARRAY MAXIMAL ADJACENT DIFFERENCE ************************/
 /*
-Given an array of integers, find the maximal absolute
-difference between any two of its adjacent elements
-Example: arr = [2, 4, 1, 0], arrayMaximalAdjacentDifference(arr) = 3
+  Given an array of integers, find the maximal absolute
+  difference between any two of its adjacent elements
+  Example: arr = [2, 4, 1, 0], arrayMaximalAdjacentDifference(arr) = 3
 */
 
+function arrayMaximalAdjacentDifference(arr) {
+  const diffs = [];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    diffs.push(Math.abs(arr[i] - arr[i + 1]));
+  }
+
+  // return diffs.reduce((max, num) => (num > max ? num : max), diffs[0]);
+  return Math.max(...diffs);
+}
+
 function arrayMaximalAdjacentDifference1(arr) {
-	// Array of absolute differences
-	const diffs = [];
+  let maxDiff = Math.abs(arr[1] - arr[0]);
 
-	for (let i = 0; i < arr.length - 1; i++) {
-		diffs.push(Math.abs(arr[i] - arr[i + 1]));
-	}
+  for (let i = 0; i < arr.length - 1; i++) {
+    const diff = Math.abs(arr[i + 1] - arr[i]);
+    if (diff > maxDiff) {
+      maxDiff = diff;
+    }
+  }
 
-	return diffs.reduce((max, num) => num > max ? num : max, diffs[0]);
+  return maxDiff;
 }
 
-
-// Another Solution
-function arrayMaximalAdjacentDifference2(arr) {
-	let maxDiff = Math.abs(arr[1] - arr[0]);
-
-	for (let i = 0; i < arr.length - 1; i++) {
-		let diff = Math.abs(arr[i + 1] - arr[i]);
-		if (diff > maxDiff) {
-			maxDiff = diff;
-		}
-	}
-	return maxDiff;
-}
-
+console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+console.log(arrayMaximalAdjacentDifference1([2, 4, 1, 0]));
+console.log(arrayMaximalAdjacentDifference([2, 4, 50, 1, 0, 10, 100]));
 console.log(arrayMaximalAdjacentDifference1([2, 4, 50, 1, 0, 10, 100]));
-console.log(arrayMaximalAdjacentDifference2([2, 4, 50, 1, 0, 10, 100]));
