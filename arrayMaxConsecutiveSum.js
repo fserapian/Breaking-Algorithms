12ed01656e5b428948af270b40255d371fff7822
Fadi Serapian <fadi.serapian@gmail.com>
2022-05-19 00:21:39 -0400

    Update arrayMaxConsecutiveSum.js

--- arrayMaxConsecutiveSum.js
+++ arrayMaxConsecutiveSum.js
@@ -1,54 +1,56 @@
+/***************** ARRAY MAX CUSECUTIVE SUM *****************/
 /*
-Given an array of integers, find the maximal possible sum
-of its k consecutive elements
-Example: arr = [1, 3, 5, 2], k = 2
+  Given an array of integers, find the maximal possible sum
+  of its k consecutive elements
+  Example: arr = [1, 3, 5, 2], k = 2
 
-* 1 + 3 = 4
-* 3 + 5 = 8
-* 5 + 2 = 7
+  * 1 + 3 = 4
+  * 3 + 5 = 8
+  * 5 + 2 = 7
 
-Thus, the answer is 8
+  Thus, the answer is 8
 */
 
 function arrayMaxConsecutiveSum(arr, k) {
-	const sums = [];
+  const sums = [];
 
-	for (let i = 0; i < arr.length - k + 1; i++) {
-		let sum = 0;
-		for (let j = i; j < k + i; j++) {
-			sum += arr[j];
-		}
-		sums.push(sum);
-	}
+  for (let i = 0; i < arr.length - k + 1; i++) {
+    let sum = 0;
+    for (let j = i; j < k + i; j++) {
+      sum += arr[j];
+    }
+    sums.push(sum);
+  }
 
-	let max = sums[0];
-	sums.forEach(sum => max = sum > max ? sum : max);
+  /*** calculate max ***/
+  // let max = sums[0];
+  // sums.forEach((sum) => (max = sum > max ? sum : max));
+  // return max;
+  /*********************/
+  return Math.max(...sums);
+}
 
-	return max;
+function arrayMaxConsecutiveSum1(arr, k) {
+  let sum = 0;
+  let max = 0;
+
+  for (let i = 0; i < k; i++) {
+    sum += arr[i];
+  }
+
+  max = sum;
+
+  for (let i = k; i < arr.length; i++) {
+    sum -= arr[i - k];
+    sum += arr[i];
+
+    if (sum > max) {
+      max = sum;
+    }
+  }
+
+  return max;
 }
 
 console.log(arrayMaxConsecutiveSum([1, 3, 5, 2], 2));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(arrayMaxConsecutiveSum1([1, 3, 5, 2], 2));
