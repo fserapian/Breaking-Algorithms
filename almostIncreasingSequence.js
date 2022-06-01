5d1806741c18694117ec8139652667eb9e250e85
Fadi Serapian <fadi.serapian@gmail.com>
2022-06-01 00:22:03 -0400

    Update almostIncreasingSequence.js

--- almostIncreasingSequence.js
+++ almostIncreasingSequence.js
@@ -1,37 +1,22 @@
-// get true if sequence is almost increasing
-// meaning that we can remove one element to get an increasing sequence
-// [1, 3, 2] === true    // we can remove 3 to get an increasing sequence
-// [1, 3, 2, 1] === false  // we can't get a sequence by removing a number
-
-function almostIncreasingSequence0(arr) { // not ideal
-	let count = 0;
-
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] <= arr[i - 1]) {
-			count++;
-		}
-		if (arr[i] <= arr[i - 2] && arr[i + 1] <= arr[i - 1]) {
-			return false;
-		}
-	}
-
-	return count <= 1;
-}
+/********************** ALMOST INCREASING SEQUENCE **********************/
+/*
+  Return true if sequence is almost increasing
+  meaning that we can remove one element to get an increasing sequence
+  Ex:
+    [1, 3, 2] === true    // we can remove 3 to get an increasing sequence
+    [1, 3, 2, 1] === false  // we can't get a sequence by removing a number
+*/
 
 function almostIncreasingSequence(arr) {
-	let count = 0;
+  let count = 0;
 
-	for (let i = 0; i < arr.length - 1; i++) {
-		if (arr[i + 1] <= arr[i]) {
-			count++;
-		}
-	}
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i + 1] <= arr[i]) {
+      count++;
+    }
+  }
 
-	if (count <= 1) {
-		return true;
-	}
-
-	return false;
+  return count <= 1;
 }
 
 console.log(almostIncreasingSequence([1, 3, 2, 1]));
