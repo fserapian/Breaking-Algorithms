b22be68644c66133e71cd2a56ae785ce093929db
Fadi Serapian <fadi.serapian@gmail.com>
2022-06-02 00:25:42 -0400

    Update allLongestStrings.js

--- allLongestStrings.js
+++ allLongestStrings.js
@@ -1,21 +1,27 @@
-// return all longest strings in an array
-// ['abcd', 'fhsj', 'dh'] === ['abcd', 'fhsj']
+/********************** ALL LONGEST STRINGS **********************/
+/*
+  Return all longest strings in an array
+  ['abcd', 'fhsj', 'dh'] === ['abcd', 'fhsj']
+*/
 
 function allLongestStrings(arr) {
-	arr.sort((a, b) => b.length - a.length);
-	const longestSize = arr[0].length;
-	return arr.filter(element => element.length === longestSize);
+  arr.sort((a, b) => b.length - a.length);
+  const longestSize = arr[0].length;
+  return arr.filter((element) => element.length === longestSize);
 }
 
-function allLongestStrings(arr) {
-	// find the longest string length
-	let longest = 0;
-	const newArr = []
-	arr.forEach(str => longest = str.length > longest ? str.length : longest);
-	arr.forEach(str => str.length === longest ? newArr.push(str) : []);
-	return newArr;
-
+function allLongestStrings1(arr) {
+  let longest = 0;
+  const newArr = [];
+  arr.forEach((str) => (longest = str.length > longest ? str.length : longest));
+  arr.forEach((str) => (str.length === longest && newArr.push(str)));
+  return newArr;
 }
 
-console.log(allLongestStrings(['bdsg', 'adsg', 'iuy', 'fhdsfs', 'fdh', 'rueysg']));
+console.log(
+  allLongestStrings(['bdsg', 'adsg', 'iuy', 'fhdsfs', 'fdh', 'rueysg'])
+);
 
+console.log(
+  allLongestStrings1(['bdsg', 'adsg', 'iuy', 'fhdsfs', 'fdh', 'rueysg'])
+);
