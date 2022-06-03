9cbf363a4092b50166fce299d2cb6b8b35c7069b
Fadi Serapian <fadi.serapian@gmail.com>
2022-06-03 00:57:20 -0400

    Update addTwoDigits.js

--- addTwoDigits.js
+++ addTwoDigits.js
@@ -1,21 +1,26 @@
-// add two digits of a number together
-// ex: addTwoDigits(24) === 6
-
-// const addTwoDigits = num => {
-// 	const a = Math.floor(num / 10);
-// 	const b = num % 10;
-// 	return a + b;
-// }
-
-// function addTwoDigits(num) {
-// 	return num.toString().split('')
-// 				.reduce((total, digit) => total + parseInt(digit), 0);
-
-// }
+/*************** ADD TWO DIGITS ***************/
+/*
+  Add two digits of a number together.
+    Ex: addTwoDigits(24) === 6
+*/
 
 function addTwoDigits(num) {
-	const digits = num.toString().split('');
-	return parseInt(digits[0]) + parseInt(digits[1]);
+  const digits = num.toString().split('');
+  return parseInt(digits[0]) + parseInt(digits[1]);
 }
 
+function addTwoDigits1(num) {
+  const firstDigit = Math.floor(num / 10);
+  const secondDigit = num % 10;
+  return firstDigit + secondDigit;
+}
+
+const addTwoDigits2 = (num) =>
+  num
+    .toString()
+    .split('')
+    .reduce((total, digit) => total + +digit, 0);
+
 console.log(addTwoDigits(58));
+console.log(addTwoDigits1(58));
+console.log(addTwoDigits2(58));
