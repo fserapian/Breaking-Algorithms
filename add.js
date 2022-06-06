6b25914ab0ef3271e1b45ae78750ddfeb03f0d70
Fadi Serapian <fadi.serapian@gmail.com>
2022-06-06 00:21:53 -0400

    Update add.js

--- add.js
+++ add.js
@@ -1,10 +1,15 @@
-// add numbers regardless of the number of arguments
-// function add() {
-// 	return Array.from(arguments).reduce((acc, num) => acc + num);
-// }
+/****************** ADD ******************/
+/*
+	Add numbers regardless of the number of arguments
+*/
 
-function add(...nums) {
-	return nums.reduce((acc, n) => acc + n);
+function add() {
+  return Array.from(arguments).reduce((acc, num) => acc + num, 0);
+}
+
+function add1(...nums) {
+  return nums.reduce((acc, n) => acc + n, 0);
 }
 
 console.log(add(3, 5, 1, 1, 5));
+console.log(add1(3, 5, 1, 1, 5));
