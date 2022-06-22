35a0ea9370c47b2f04c747f983b0c054f56727e8
Fadi Serapian <fadi.serapian@gmail.com>
2022-06-22 00:06:39 -0400

    Update addBorder.js

--- addBorder.js
+++ addBorder.js
@@ -1,41 +1,30 @@
-// add a border around the array (build a wall)
-// picture = ["abc",
-//            "ded" ]  ===  
-// picture = ["****",
-//            "*abc*",
-//            "*ded*",
-//            "*****"]
+/********************* ADD BORDER *********************/
+/*
+  Add a border around the array (build a wall)
+  picture = ["abc",
+            "ded" ]
 
-// 1st attempt
-// function addBorder(arr) {
-// 	arr.unshift('***');
-// 	arr.push('***');
-// 	const newArr = arr.map(element => '*'.concat(element.concat('*')));
-// 	return newArr;
-// }
+  =>
 
-// function addBorder(arr) {
-// 	// size of the wall
-// 	const wallSize = arr[0].length + 2;
-// 	let wall = '';
-// 	for (let i = 0; i < wallSize; i++) {
-// 		wall += '*';
-// 	}
-// 	arr.unshift(wall);
-// 	arr.push(wall);
-
-// 	return arr.map(element => element[0] !== '*' ? '*'.concat(element, '*') : element);
-// }
+  picture = ["****",
+             "*abc*",
+             "*ded*",
+             "*****"]
+*/
 
 function addBorder(arr) {
-	const wall = '*'.repeat(arr[0].length + 2);
+  const wall = '*'.repeat(arr[0].length + 2);
 
-	arr.unshift(wall);
-	arr.push(wall);
-	for (let i = 1; i < arr.length - 1; i++) {
-		arr[i] = '*'.concat(arr[i], '*');
-	}
-	return arr;
+  arr.unshift(wall);
+  arr.push(wall);
+
+  for (let i = 1; i < arr.length - 1; i++) {
+    // arr[i] = '*'.concat(arr[i], '*');
+    // arr[i] = '*' + arr[i] + '*';
+    arr[i] = `*${arr[i]}*`;
+  }
+
+  return arr;
 }
 
-console.log(addBorder(["abc", "def"]));
+console.log(addBorder(['abc', 'def']));
